3c715f16afc78cbff42f3cc176768490c8bd7627
Pedro <me@axyria.dev>
2023-01-01 21:09:10 -0300

    fix: eslint header

--- secrets/backend.env.mjs
+++ secrets/backend.env.mjs
@@ -1,6 +1,26 @@
+/*
+ * Hearkus, a free and open-source platform for discovering and sharing
+ * feedback on music.
+ *
+ * Copyright (c) 2022-2023 Pedro Henrique
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { findEnv, parse } from './shared.env.mjs';
 
-const BackendEnvSchema = (z) =>
+const BackendEnvSchema = z =>
   z.object({
     // NODE_ENV: z.enum(['development', 'test', 'production']),
     BACKEND_PORT: z.string(),
@@ -9,4 +29,4 @@ const BackendEnvSchema = (z) =>
 
 const env = findEnv();
 
-export default (z) => parse(BackendEnvSchema(z), env);
+export default z => parse(BackendEnvSchema(z), env);
